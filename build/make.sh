916d5305129532dfba0e7dfbac0a6459ad56962f
peccu <peccul@gmail.com>
2022-08-30 12:21:56 +0000

    add style

--- build/make.sh
+++ build/make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SCPATH=$(cd $(dirname $0);pwd)
 cat $SCPATH/head.html \
-  <(ls -t1 public/images | xargs -I{} echo '    <img src="./images/{}">') \
+  <(ls -t1 public/images | xargs -I{} echo '    <div class="item"><div class="name">{}</div><div class="image"><img src="./images/{}"/></div></div>') \
   <(echo ' </body>') \
   <(echo '</html>') \
   > public/index.html

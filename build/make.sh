2b1e882631f58dca4a9e9d4d3b63c8859836fbcd
peccu <peccul@gmail.com>
2022-08-31 02:40:21 +0000

    remove unused node implementation

--- build/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-SCPATH=$(cd $(dirname $0);pwd)
-cat $SCPATH/head.html \
-  <(ls -t1 public/images | xargs -I{} echo '    <img src="./images/{}">') \
-  <(echo ' </body>') \
-  <(echo '</html>') \
-  > public/index.html
